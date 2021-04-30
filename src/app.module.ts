a77442129829dab7ee5023c02976b2557bfc4580
Wilk Franca <wilk.caetano@gmail.com>
2021-04-30 10:26:20 -0300

    create crud movies

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './core/database/database.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AuthModule } from './auth/auth.module';
     DatabaseModule,
     UsersModule,
     AuthModule,
+    MoviesModule,
   ],
   controllers: [],
   providers: [],
